77deb775404841670953520c34849515f7cce44a
Tuyen_Lang <2200010034@nttu.edu.vn>
2025-09-21 20:51:43 +0700

    custome common UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,24 @@ import { Label } from "@/components/ui/label";
 import { TableBody } from "@/components/ui/table";
 import Image from "next/image";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
   return (
-   <div>
+   <div className=" flex flex-col gap-y-4">
       <Label>HELLO WELCOME PAGE</Label>
-      <Button className="">LICK ME</Button>
-      <TableBody>
-        <li>A</li>
-        <li>A</li>
-        <li>A</li>
-      </TableBody>
+      <Input placeholder="enter" ></Input>
+      <Textarea placeholder={"I am here"}></Textarea>
+      <Button variant={"elevated"} >LICK ME</Button>
+      <Checkbox value={"checked"}></Checkbox>
+      <Progress value={50}></Progress>
+      <div>
+        <Button variant={"elevated"}>Lick</Button>  
+      </div>   
    </div>
+   
   );
 }
